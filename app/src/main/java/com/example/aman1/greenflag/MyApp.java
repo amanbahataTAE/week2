033d93ab0ff1c34b33e850b0ef06ad302c7f9bd1
AmanBahataandroid <amanbahata.test@gmail.com>
2017-11-20 18:09:38 +0000

    Adds build

--- app/src/main/java/com/example/aman1/greenflag/MyApp.java
+++ app/src/main/java/com/example/aman1/greenflag/MyApp.java
@@ -1,6 +1,12 @@
 package com.example.aman1.greenflag;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.content.pm.SharedLibraryInfo;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.SecureRandom;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -14,6 +20,20 @@ public class MyApp extends Application{
     public void onCreate(){
         super.onCreate();
 
+
+        SharedPreferences preferences = getApplicationContext().getSharedPreferences("HEY_STORED", MODE_PRIVATE);
+        SharedPreferences.Editor edit = preferences.edit();
+        byte[] key;
+        if (preferences.getString("DB_KEY", null) == null){
+            key = new byte[64];
+            new SecureRandom().nextBytes(key);
+            edit.putString("DB_KEY", Base64.encodeToString(key,Base64.DEFAULT));
+
+            edit.commit();
+        }else{
+            key = Base64.decode( preferences.getString("DB_KEY",null),Base64.DEFAULT);
+        }
+
         /**
          *
          */
@@ -25,6 +45,7 @@ public class MyApp extends Application{
         RealmConfiguration configuration = new RealmConfiguration.Builder()
                 .name(Realm.DEFAULT_REALM_NAME)
                 .schemaVersion(1)
+                .encryptionKey(key)
                 .deleteRealmIfMigrationNeeded()
                 .build();
 
